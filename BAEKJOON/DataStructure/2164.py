dd8b8a5ca531b3847d772b54fac9d2274cde8b0d
sikkky <sungsik.yang92@gmail.com>
2021-06-16 23:32:30 +0900

    백준 카드 - 큐를 사용하면 됨!

---
+++ BAEKJOON/DataStructure/2164.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+
+inp = int(input())
+queue = deque()
+
+for i in range(1, inp+1):
+    queue.append(i)
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue.pop())
