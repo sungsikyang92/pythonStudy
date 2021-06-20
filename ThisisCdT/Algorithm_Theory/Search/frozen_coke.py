8668c60043e23253649338bdb26094adb26af3fa
sikkky <sungsik.yang92@gmail.com>
2021-06-20 17:26:18 +0900

    검색 - 음료수 얼려 먹기 dfs

---
+++ ThisisCdT/Algorithm_Theory/Search/frozen_coke.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+tray = []
+for i in range(n):
+    tray.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if tray[x][y] == 0:
+        tray[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
